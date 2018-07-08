ed03ea7fca42ca61f49b9045a102bc90dc67b375
Gitair2016 <wj_rise99@163.com>
2018-07-08 18:28:41 +0800

    implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
+      remember @user
       flash[:success] = "welcome sign up the accont"
       redirect_to user_url(@user)
     else
